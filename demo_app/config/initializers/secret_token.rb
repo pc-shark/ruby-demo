be70c4b739e172c879d14fd9645218763472e593
ruiminz <ruiminz@mtu.edu>
2013-02-13 19:51:37 -0500

    init

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2a15a800a76a85478532e0206e8ddf8410c268e7db4e03e0b60c84985fc87c64caff2fcb9f9e31ca0d6d08ff96b0e66fcf77b46f13cf83821990a4e593555874'
---
+++ first-app/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '57e713ce9c02518fdd8c6eac68ec1b159665569f2021d81d63687a8077e0af637a055281f88edd8fb09d805e0ffd36c141e246779b85ddb45f68d388deb57421'
